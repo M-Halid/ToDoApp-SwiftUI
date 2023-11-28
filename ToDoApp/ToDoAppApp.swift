ef0dbb9953711ac60015980d1e7964b7c83794d4
Muhammed Halid Kutsal <mhalidkutsal@gmail.con>
2023-11-28 15:31:07 +0100

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Muhammed Halid Kutsal on 28.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
